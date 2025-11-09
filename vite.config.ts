8e68070f73bad419a4d07dded7016ed344264328
Gitpagess <info@4seasonsheating.com>
2025-11-08 22:07:27 -0800

    Add comment for base path in vite.config.ts
    Added comment to explain the base path for GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/inventory-manager/'
+  base: '/inventory-manager/',   // <-- important for GitHub Pages
+  server: { host: true }
 })
