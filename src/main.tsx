41511ae5a544959bfcfb38ba3c3704c3d245cad5
Gitpagess <info@4seasonsheating.com>
2025-11-08 22:37:08 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import AuthGate from './AuthGate'
+import { AuthGate } from './auth'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
