51afd40d73ed9ee9b297a1fd4f44a39a74f130de
Gitpagess <info@4seasonsheating.com>
2025-11-08 22:45:09 -0800

    Refactor AuthGate import and usage in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import { AuthGate } from './auth'
+import AuthGate from './AuthGate'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate>
-      <App />
-    </AuthGate>
+    <AuthGate />
   </React.StrictMode>
 )
