69c8e37b21af5e0149faeb2d6bee8ab2de1d31c9
Gitpagess <info@4seasonsheating.com>
2025-11-08 23:32:19 -0800

    Replace AuthGate component with App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AuthGate from './AuthGate'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate />
+    <App />
   </React.StrictMode>
 )
